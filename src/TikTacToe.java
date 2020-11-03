8f8c936e49c62ad9e86f8c613ab09b2c923bc840
Vikash Kanna <Enter Git Registered Email id>
2020-11-03 17:20:15 +0530

    UC1 Task Completed

---
+++ src/TikTacToe.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Tictactoe {
+    static char[] board = new char[10];
+    private static String compchoice;
+    public static void main(String args[]) {
+        System.out.println("Welcome to Tictactoe Game---");
+        Board();
+        Inputchoice();
+
+    }
+
+
+    private static void Inputchoice() {
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Choose 'X' OR 'O' as input: ");
+        String playchoice = inp.nextLine();
+        if(playchoice.equals('X' )) {
+            compchoice = "O";
+        }
+        else if(playchoice.equals('O')){
+            compchoice = "X";
+        }
+        else {
+            System.out.println("Wrong input");
+            Inputchoice();
+        }
+        
